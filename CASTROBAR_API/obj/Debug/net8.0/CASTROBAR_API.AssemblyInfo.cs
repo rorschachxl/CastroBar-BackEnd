01c69b5f9c92ef8e6f88c6d798e0304dca11db36
Gennier Caro <gennier002@gmail.com>
2024-10-01 07:35:14 -0500

    Añadidas las carpetas Dtos, Repositories, Models, Services y Utilities

--- CASTROBAR_API/obj/Debug/net8.0/CASTROBAR_API.AssemblyInfo.cs
+++ CASTROBAR_API/obj/Debug/net8.0/CASTROBAR_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CASTROBAR_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a702c726caad865541c8a94708e9afdff40aec9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CASTROBAR_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CASTROBAR_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
