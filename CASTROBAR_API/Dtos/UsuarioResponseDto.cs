81e5e1cc7c70463819ff4fb5b118795e44017f46
AlejandroFlorez1 <alejofl2003@gmail.com>
2024-10-02 18:47:30 -0500

    2/10/2024 AGREGAR EL USUARIORESPONSEDTO

---
+++ CASTROBAR_API/Dtos/UsuarioResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CASTROBAR_API.Dtos
+{
+    public class UsuarioResponseDto
+    {
+        public string numeroDocumento { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+    }
+}
